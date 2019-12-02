314021f5a0788f2ff850df7a3330ab8c26b3d939
vmikhaylov <vmikhaylov@ourgapps.com>
2019-12-02 11:52:33 +0300

    review changes

--- hw_1/main.go
+++ hw_1/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/beevik/ntp"
-	"os"
+	"log"
 )
 
 func main() {
-	if time, error := ntp.Time("0.beevik-ntp.pool.ntp.org"); error == nil {
-		fmt.Println(time)
+	if time, err := ntp.Time("0.beevik-ntp.pool.ntp.org"); err != nil {
+		log.Fatal(err.Error())
 	} else {
-		os.Stderr.WriteString(error.Error())
-		os.Exit(1)
+		fmt.Println(time)
 	}
 }
