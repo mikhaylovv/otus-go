cc2b90d2fc19c8a00a9040411c234a485f2bd47c
vmikhaylov <vmikhaylov@ourgapps.com>
2020-02-06 14:50:53 +0300

    linux specific test sh

--- hw_6/test/test.sh
+++ hw_6/test/test.sh
@@ -1,5 +1,5 @@
 go build ../main.go
-./main.exe --from=test.in --to=test.out --limit=33 --offset=11
+./main --from=test.in --to=test.out --limit=33 --offset=11
 diff test.out expected.out >&2
 if [ $? -ne 0 ]
 then
@@ -7,7 +7,7 @@ then
   exit 1
 fi
 
-./main.exe --from=test.in --to=test.out --limit=33
+./main --from=test.in --to=test.out --limit=33
 if [ $? -ne 0 ]
 then
   echo "test failed, files noe equal" >&2
