fbfe592245b03cf44c3ae3fc85cce18850434107
vmikhaylov <vmikhaylov@ourgapps.com>
2020-02-06 14:46:00 +0300

    cmd run errors at stderr

--- hw_6/copy_test.go
+++ hw_6/copy_test.go
@@ -10,7 +10,7 @@ import (
 func TestCopyProg(t *testing.T) {
 	cmd := exec.Command("sh", "test.sh")
 	cmd.Dir = "test"
-	cmd.Stdout = os.Stdout
+	cmd.Stdout = os.Stderr
 
 	err := cmd.Run()
 	if err != nil {
