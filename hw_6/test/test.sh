c68b442177179d00def7fea1dabab1581e2bf9ef
vmikhaylov <vmikhaylov@ourgapps.com>
2020-02-06 16:02:31 +0300

    test case in CI fixed

--- hw_6/test/test.sh
+++ hw_6/test/test.sh
@@ -1,16 +1,17 @@
 go build ../main.go
-./main --from=test.in --to=test.out --limit=33 --offset=11
+
+./main --from=test.in --to=test.out --limit=33
 diff test.out expected.out
 if [ $? -ne 0 ]
 then
-  echo "test failed, files noe equal"
+  echo "test failed, files not equal"
   exit 1
 fi
 
-./main --from=test.in --to=test.out --limit=33
+./main --from=test.in --to=test.out --limit=33 --offset=10
+diff test.out expected.out
 if [ $? -ne 0 ]
 then
-  echo "test failed, files noe equal"
+  echo "test failed, files not equal"
   exit 2
 fi
-diff test.out expected.out
