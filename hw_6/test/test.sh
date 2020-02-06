5bfd608ddc8b4b643925d6b2541f6c70dd6719c7
vmikhaylov <vmikhaylov@ourgapps.com>
2020-02-06 15:55:06 +0300

    test sh fix cout

--- hw_6/test/test.sh
+++ hw_6/test/test.sh
@@ -1,16 +1,16 @@
 go build ../main.go
 ./main --from=test.in --to=test.out --limit=33 --offset=11
-diff test.out expected.out >&2
+diff test.out expected.out
 if [ $? -ne 0 ]
 then
-  echo "test failed, files noe equal" >&2
+  echo "test failed, files noe equal"
   exit 1
 fi
 
 ./main --from=test.in --to=test.out --limit=33
 if [ $? -ne 0 ]
 then
-  echo "test failed, files noe equal" >&2
+  echo "test failed, files noe equal"
   exit 2
 fi
 diff test.out expected.out
