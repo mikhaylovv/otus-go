30cfbd8d427c03a0d47e9d1df5e97d57d2b706e5
vmikhaylov <vmikhaylov>
2020-03-12 15:30:28 +0300

    go vet fix

--- hw_8/calendar/calendar.go
+++ hw_8/calendar/calendar.go
@@ -2,6 +2,7 @@ package calendar
 
 import "github.com/mikhaylovv/otus-go/hw_8/calendar/storage"
 
+// Calendar - base structure for Calendar micro service
 type Calendar struct {
 	Storage storage.Storage
 }
