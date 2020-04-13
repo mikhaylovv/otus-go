3a09807661705209e4e4772b9eb7cdc61b31f35d
vmikhaylov <vmikhaylov>
2020-04-13 12:16:32 +0300

    main -> cmd/main, mutex in inmemmorystorage, CI fixes

--- hw_8/calendar/storage/inmemorystorage/in_memory_storage.go
+++ hw_8/calendar/storage/inmemorystorage/in_memory_storage.go
@@ -2,6 +2,7 @@ package inmemorystorage
 
 import (
 	"github.com/mikhaylovv/otus-go/hw_8/calendar/storage"
+	"sync"
 	"time"
 )
 
@@ -9,6 +10,7 @@ import (
 type InMemoryStorage struct {
 	events map[uint]storage.Event
 	last   uint
+	mutex  sync.RWMutex
 }
 
 // NewInMemoryStorage - construct NewInMemoryStorage object
@@ -22,35 +24,48 @@ func NewInMemoryStorage() *InMemoryStorage {
 // AddEvent - add new event in Storage or error ErrEventAlreadyExist
 func (s *InMemoryStorage) AddEvent(e storage.Event) (uint, error) {
 	idx := s.last
+	s.mutex.Lock()
 	e.ID = idx
 	s.events[idx] = e
 	s.last++
+	s.mutex.Unlock()
 	return idx, nil
 }
 
 // DeleteEvent - delete existing event from storage
 func (s *InMemoryStorage) DeleteEvent(id uint) error {
+	s.mutex.Lock()
 	delete(s.events, id)
+	s.mutex.Unlock()
 	return nil
 }
 
 // ChangeEvent - changes existing event. If event not found send ErrEventNotFound
 func (s *InMemoryStorage) ChangeEvent(new storage.Event) error {
+	s.mutex.RLock()
 	if _, ok := s.events[new.ID]; ok {
+		s.mutex.RUnlock()
+		s.mutex.Lock()
 		s.events[new.ID] = new
+		s.mutex.Unlock()
 		return nil
 	}
+
+	s.mutex.RUnlock()
 	return storage.ErrEventNotFound
 }
 
 // GetEvents - gets events from date to date, error always nil
 func (s *InMemoryStorage) GetEvents(from time.Time, to time.Time) ([]storage.Event, error) {
 	var res []storage.Event
+
+	s.mutex.Lock()
 	for _, ev := range s.events {
 		if ev.Date.After(from) && ev.Date.Before(to) {
 			res = append(res, ev)
 		}
 	}
+	s.mutex.Unlock()
 
 	return res, nil
 }
